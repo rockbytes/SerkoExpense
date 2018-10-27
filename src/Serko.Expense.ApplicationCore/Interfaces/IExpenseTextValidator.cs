953ff0c5163aceaafc0a050c30e57a46d1dac04e
Xu Xie <Xu.Xie@softwareofexcellence.com>
2018-10-27 23:28:00 +1300

    Add validations for input expense text. Add unit tests for the validations.

---
+++ src/Serko.Expense.ApplicationCore/Interfaces/IExpenseTextValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serko.Expense.ApplicationCore.Interfaces
+{
+    public interface IExpenseTextValidator
+    {
+        bool Validate(string expenseText);
+    }
+}
---
+++ src/Serko.Expense.ApplicationCore/Services/XmlExpenseTextValidator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Serko.Expense.ApplicationCore.Interfaces;
+
+namespace Serko.Expense.ApplicationCore.Services
+{
+    public class XmlExpenseTextValidator : IExpenseTextValidator
+    {
+        public bool Validate(string expenseText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateMatchOfOpeningClosingTags(string xmlText)
+        {
+            if (xmlText == null)
+            {
+                return true;
+            }
+
+            var xmlRegex = new Regex(@"\<(/?\w+)\>");
+            var matches = xmlRegex.Matches(xmlText);
+
+            var xmlTags = new Stack<string>();
+            foreach (Match match in matches)
+            {
+                var tag = match.Groups[1].ToString();
+                if (!tag.StartsWith("/"))
+                {
+                    xmlTags.Push(tag);
+                }
+                else
+                {
+                    if (xmlTags.Count == 0 ||
+                        !tag.Equals($"/{xmlTags.Pop()}", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return xmlTags.Count == 0;
+        }
+
+        public bool ValidatePresenceOfTotalTag(string xmlText)
+        {
+            return xmlText != null && xmlText.Contains("<total>");
+        }
+    }
+}
